ef4eedec22028b7caaffe8ded902a9ce84a30bb2
luissb94 <luissellesb94@gmail.com>
2018-05-23 18:00:03 +0200

    V0.08 - Changed the loop of the game. Now shows which player must dice roll and move chip.

--- FinalProjectLudo/FinalProjectLudo/LudoGame.cs
+++ FinalProjectLudo/FinalProjectLudo/LudoGame.cs
@@ -2,9 +2,11 @@
 //V0.03 -  Creating the Main class of the game Ludo
 //          Here will be the whole code of the game.
 //V0.07 -  Added array of boxes.
+//V0.08 -  Added list of player
 
 using System;
 using Tao.Sdl;
+using System.Collections.Generic;
 
 namespace FinalProjectLudo
 {
@@ -33,6 +35,9 @@ namespace FinalProjectLudo
 
         public void PlayGame()
         {
+            List<Player> player = new List<Player>();
+            player = playSelect.GetPlayerList();
+
             bool exit = false;
             string arrayData = "files/boxArrayData.txt";
             playSelect.Show();
@@ -43,32 +48,36 @@ namespace FinalProjectLudo
                     "Enter the roll: ", red);
             //Main loop of the ludo game. It will show a menu, step by step
             //to show the player hoy to play.
-            do
+            exit = false;
+            foreach (Player p in player)
             {
-
+                //1 - Write lines.
                 int numChip, rollValue;
-                exit = false;
                 hardware.ClearScreen();
                 hardware.DrawImage(imgLudo);
-                
-                //hardware.WriteText(txtNames, 800, 100);
+                txtDev = SdlTtf.TTF_RenderText_Solid(font.GetFontType(),
+                "Enter the roll: ", red);
+                txtNames = SdlTtf.TTF_RenderText_Solid(font.GetFontType(),
+                "Turn:  "+ p.GetName() +"            Color: "+ p.GetColor(), red);
+
+                hardware.WriteText(txtNames, 650, 20);
 
                 //I used this here just to make sure they work.
                 //boxes.UploadToFtp();
                 //boxes.DownloadFromFtp();
-
+                
 
                 hardware.UpdateScreen();
 
                 //Commented because of the devRoll.
                 /*menu.ShowFirstStep();
-                
+
 
                 do
                 {
                     //Repeat until press 1
                 } while (hardware.KeyPressed() != Hardware.KEY_1);
-                
+
                 //Shows the image of the rollvalue.
                 switch(dice.GetRollValue())
                 {
@@ -112,10 +121,10 @@ namespace FinalProjectLudo
 
                 hardware.WriteText(txtDev, 640, 330);
                 hardware.UpdateScreen();
-               
+
 
                 rollValue = dice.GetDevRoll();
-                
+
 
                 menu.ShowSecondStep();
 
@@ -126,9 +135,8 @@ namespace FinalProjectLudo
                     {
                         exit = true;
                     }
-                } while(!exit);
-                
-            } while (!exit);
+                } while (!exit && !p.GetWin());
+            }
         }
     }
 }
