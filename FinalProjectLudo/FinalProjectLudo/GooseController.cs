bf4747c5a0afe89c03071bfb228e75c866307ba4
luissb94 <luissellesb94@gmail.com>
2018-05-25 18:45:42 +0200

    V0.10 - Added background image

--- FinalProjectLudo/FinalProjectLudo/GooseController.cs
+++ FinalProjectLudo/FinalProjectLudo/GooseController.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Luis Selles
+//V0.10 -  Created the last static content.
 
+using System;
+using System.Collections.Generic;
 namespace FinalProjectLudo
 {
     class GooseController
     {
+        protected Image imgGoose, imgDice;
+        protected Hardware hardware;
+        protected IntPtr textSpace;
+        protected PlayerSelect playSelect;
+        protected Dice dice;
+        protected Box boxes;
+        protected Chip chip;
+        protected BoxProperties[] arrayBox = new BoxProperties[100];
+        protected List<Chip> chipslist = new List<Chip>();
+
+        public GooseController(Hardware hardware)
+        {
+            imgGoose = new Image("img/gooseBoard.jpg", 650, 650);
+            imgGoose.MoveTo(0, 0);
+            this.playSelect = new PlayerSelect(hardware);
+            this.hardware = hardware;
+            this.dice = new Dice(hardware);
+            this.boxes = new Box();
+            this.chip = new Chip();
+            this.chipslist = chip.Load();
+        }
+
+        public void Play()
+        {
+            hardware.ClearScreen();
+            hardware.DrawImage(imgGoose);
+            hardware.UpdateScreen();
+            int key;
+
+            do
+            {
+                key = hardware.KeyPressed();
+            } while (key != Hardware.KEY_ESC);
+            
+        }
     }
 }
